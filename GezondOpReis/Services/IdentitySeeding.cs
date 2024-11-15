f4d2c023353be3c60587cf1139e85882ad2f1a2e
VanDesselSam <r0743194@student.thomasmore.be>
2024-11-15 13:19:29 +0100

    identity seeding class toegevoegd met user en admin

---
+++ GezondOpReis/Services/IdentitySeeding.cs
@@ -0,0 +1,25 @@
+﻿using System.Data.Common;
+
+namespace GezondOpReis.Services
+{
+	public class IdentitySeeding
+	{
+		public async Task RoleSeedingAsync(RoleManager<IdentityRole> roleManager)
+		{
+			try
+			{
+				//User
+				bool role = await roleManager.RoleExistsAsync("user");
+				if (!role) await roleManager.CreateAsync(new IdentityRole("user"));
+
+				//Admin
+				role = await roleManager.RoleExistsAsync("admin");
+				if (!role) await roleManager.CreateAsync(new IdentityRole("admin"));
+			}
+			catch (DbException ex)
+			{
+				throw new Exception(ex.Message.ToString());
+			}
+		}
+	}
+}
