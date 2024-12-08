d145f24d091a7803db1bebe04d9b4a76a8556827
Niels2580 <r0831355@student.thomasmore.be>
2024-12-08 17:33:33 +0100

    Datum controle + CamelCase aangepast

--- GezondOpReisMVC/ViewModels/OpleidingEditViewModel.cs
+++ GezondOpReisMVC/ViewModels/OpleidingEditViewModel.cs
@@ -2,11 +2,26 @@
 {
     public class OpleidingEditViewModel
     {
-        public int  Id { get; set; }
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Naam is verplicht.")]
         public string Naam { get; set; }
+
+        [Required(ErrorMessage = "Beschrijving is verplicht.")]
         public string Beschrijving { get; set; }
+
+        [Required(ErrorMessage = "Startdatum is verplicht.")]
+        [Display(Name = "Startdatum")]
         public DateTime StartDatum { get; set; }
+
+        [Required(ErrorMessage = "Einddatum is verplicht.")]
+        [Display(Name = "Einddatum")]
+        [ValidateEndDate("StartDatum", ErrorMessage = "Einddatum moet na de startdatum liggen.")]
         public DateTime EindDatum { get; set; }
+
+        [Required(ErrorMessage = "Aantal plaatsen is verplicht.")]
+        [Range(1, int.MaxValue, ErrorMessage = "Aantal plaatsen moet groter zijn dan nul.")]
+        [Display(Name = "Aantal plaatsen")]
         public int AantalPlaatsen { get; set; }
     }
 }
