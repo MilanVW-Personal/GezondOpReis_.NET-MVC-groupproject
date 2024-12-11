e72cefdf6ffff9a98c8e9174696ff3b1f5e26b85
MilanVW1 <r0985432@student.thomasmore.be>
2024-12-11 09:50:01 +0100

    dashboard voor deelnemer afgewerkt + vorige groepsreizen aangepast zodat deze enkel de reizen toont waarvan de huidige datum niet groter is dan de einddatum, een maand later.

--- GezondOpReisMVC/Controllers/DashboardController.cs
+++ GezondOpReisMVC/Controllers/DashboardController.cs
@@ -16,6 +16,11 @@ namespace GezondOpReis.Controllers
 
         public async Task<IActionResult> Index()
         {
+            // Als user niet is ingelogd, redirecten naar inlog pagina
+            if (!User.Identity.IsAuthenticated)
+                return RedirectToAction("Login", "Gebruiker");
+
+
             var user = await _userManager.FindByNameAsync(User.Identity.Name);
 
             if (user == null)
@@ -25,13 +30,15 @@ namespace GezondOpReis.Controllers
             var vorigeReizen = await _unitOfWork.GroepsReisRepository.GetVorigeReizen(user.Id); // Deze methode zal alle reizen, waarop je bent ingeschreven, ophalen waarvan de einddatum in het verleden ligt. 
             var ingeschrevenReizen = await _unitOfWork.GroepsReisRepository.GetIngeschrevenGroepsreizen(user.Id);
 
+
+            var reizenInVerleden = vorigeReizen.Where(r => !(DateTime.Now > r.EindDatum.AddMonths(1))); // de reizen in het verleden zijn de vorige ingeschreven reizen, waarvan de huidige datum niet groter is dan de einddatum, een maand later.
+
             var vm = new DashboardViewModel
             {
-                VorigeReizen = vorigeReizen.ToList(),
+                ReizenInVerleden = reizenInVerleden.ToList(),
                 IngeschrevenReizen = ingeschrevenReizen.ToList(),
                 Kinderen = kinderen.ToList()
             };
-            
 
             return View(vm);
         }
